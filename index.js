bc1c58b65830b22023c2b709ecf4ebcb94424c37
abrar2139 <mabrarqadri3@mail.com>
2024-03-11 15:02:04 +0500

    route is changed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app =express();
 port = 5000
 
 
-app.use("/", (req, res)=>{
+app.use("/api/lines", (req, res)=>{
     res.send(`Server is up and listening on port ${port}, Server is up and listening on port ${port}, Server is up and listening on port ${port}, Server is up and listening on port ${port},Server is up and listening on port ${port}m, Server is up and listening on port ${port}, Server is up and listening on port ${port}, Server is up and listening on port ${port}, Server is up and listening on port ${port}, Server is up and listening on port ${port}, Server is up and listening on port ${port}, Server is up and listening on port ${port},  ` )
     res.send(`Server is up and listening on port ${port}, Server is up and listening on port ${port}, Server is up and listening on port ${port}, Server is up and listening on port ${port},Server is up and listening on port ${port}m, Server is up and listening on port ${port}, Server is up and listening on port ${port}, Server is up and listening on port ${port}, Server is up and listening on port ${port}, Server is up and listening on port ${port}, Server is up and listening on port ${port}, Server is up and listening on port ${port}, ,  ` )
 })
